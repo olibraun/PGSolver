41cffd9c69a031804ed094600aa0daac9010aa71
olibraun <oliverbraun89@gmail.com>
2018-04-20 07:12:37 +0200

    feat: Printing the results and checking for incorrect input.

--- solver.js
+++ solver.js
@@ -3,7 +3,7 @@ let max = 0;
 let avg = 0;
 let ich = 0;
 
-const eps = 0.01;
+const eps = 0.1;
 
 let valid_possibilities = [];
 
@@ -11,27 +11,41 @@ const feld_min = document.getElementById('min');
 const feld_max = document.getElementById('max');
 const feld_ich = document.getElementById('ich');
 const feld_avg = document.getElementById('avg');
+const ul = document.querySelector('ul');
 
 function run() {
+  ul.innerHTML = '';
   min = Number(feld_min.value);
   max = Number(feld_max.value);
   ich = Number(feld_ich.value);
   avg = Number(feld_avg.value);
-  console.log('Start run with ', min, max, ich, avg);
-  for (let i = min; i <= max; i++) {
-    for (let j = i; j <= max; j++) {
-      for (let k = j; k <= max; k++) {
-        for (let l = k; l <= max; l++) {
-          for (let m = l; m <= max; m++) {
-            for (let n = m; n <= max; n++) {
-              for (let o = n; o <= max; o++) {
-                const array = [i, j, k, l, m, n, o, ich];
-                console.log('Testing array ', array);
-                const current_avg = get_average(array);
-                console.log('current_avg: ', current_avg);
-                if (Math.abs(current_avg - avg) < eps) {
-                  console.log('Solution found');
-                  valid_possibilities.push(array);
+  if(min > max || avg > max || ich < min || ich > max){
+    console.error('Eingabefehler.');
+    const li = document.createElement('li');
+    const itemText = document.createTextNode('Eingabedaten fehlerhaft.');
+    li.appendChild(itemText);
+    ul.appendChild(li);
+  } else {
+    console.log('Start run with ', min, max, ich, avg);
+    for (let i = min; i <= max; i++) {
+      for (let j = i; j <= max; j++) {
+        for (let k = j; k <= max; k++) {
+          for (let l = k; l <= max; l++) {
+            for (let m = l; m <= max; m++) {
+              for (let n = m; n <= max; n++) {
+                for (let o = n; o <= max; o++) {
+                  const array = [i, j, k, l, m, n, o, ich];
+                  console.log('Testing array ', array);
+                  const current_avg = get_average(array);
+                  //console.log('current_avg: ', current_avg);
+                  if (Math.abs(current_avg - avg) < eps) {
+                    console.log('Solution found');
+                    valid_possibilities.push(array);
+                    const li = document.createElement('li');
+                    const itemText = document.createTextNode(array + ' ergibt ' + current_avg);
+                    li.appendChild(itemText);
+                    ul.appendChild(li);
+                  }
                 }
               }
             }
@@ -39,8 +53,8 @@ function run() {
         }
       }
     }
+    console.log(valid_possibilities);
   }
-  console.log(valid_possibilities);
 }
 
 function get_average(arr) {
